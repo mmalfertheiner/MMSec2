d71ea881396f2f6695a8a2a79ed8bc81580e9fdc
martin <martin.malfertheiner@gmail.com>
2015-04-17 15:18:00 +0200

    First version of equal error rate.

--- e1.r
+++ e1.r
@@ -7,8 +7,17 @@ data <- read.table("question1.txt", sep=",", header=FALSE)
 pred <- prediction(data[,1],data[,2])
 perf <- performance(pred, measure = "tpr", x.measure = "fpr")
 
+
 png(filename="roc.png")
 plot(perf, col="blue")
 abline(0,1,col="grey")
 abline(1,-1,col="red")
 
+dev.off()
+
+df <- with(attributes(pred), data.frame(cutoffs=cutoffs[[1]], tp=tp[[1]], fn=fn[[1]], tn=tn[[1]], fp=fp[[1]], TMR=tp[[1]]/(tp[[1]]+fn[[1]]), TNMR=tn[[1]]/(tn[[1]]+fp[[1]])))
+
+i_eer <- with(df, which.min(abs(TMR-TNMR)))
+EER <- with(df[i_eer,], mean(c(TMR,TNMR)))
+
+print(EER)
