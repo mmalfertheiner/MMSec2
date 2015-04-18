9e09488d3dd46773679dd02efdcd83f70b029578
martin <martin.malfertheiner@gmail.com>
2015-04-18 10:51:11 +0200

    Done first exercise.

--- e1.r
+++ e1.r
@@ -4,14 +4,15 @@ setwd("/home/martin/Projects/MMSec2")
 
 data <- read.table("question1.txt", sep=",", header=FALSE)
 
-pred <- prediction(data[,1],data[,2])
+#Label ordering: negative label first, positive label second
+pred <- prediction(data[,1],data[,2], c("cover", "stego"))
 perf <- performance(pred, measure = "tpr", x.measure = "fpr")
 
 
 png(filename="roc.png")
 plot(perf, col="blue")
-abline(0,1,col="grey")
-abline(1,-1,col="red")
+abline(0,1, col="grey")
+abline(1,-1, col="red")
 
 dev.off()
 
@@ -20,5 +21,7 @@ df <- with(attributes(pred), data.frame(tp=tp[[1]], fn=fn[[1]], tn=tn[[1]], fp=f
 
 i_eer <- with(df, which.min(abs(TMR-TNMR)))
 EER <- with(df[i_eer,], mean(c(TMR,TNMR)))
+
+#Result: Equal error rate: 0.953125
 
 print(EER)
